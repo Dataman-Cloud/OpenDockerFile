f5ff132c6474f3a7e46bd9d1abe297a3b2e190ee
ss75710541 <75710541@qq.com>
2016-07-12 19:31:12 +0800

    update zookeeper

---
+++ centos7-jdk8/build.sh
@@ -0,0 +1,1 @@
+docker build $opts -t library/centos7-jdk8 .
--- zookeeper/docker/dataman_zookeeper.sh
+++ zookeeper/docker/dataman_zookeeper.sh
@@ -8,7 +8,7 @@ if [ ! -z "$ZKLIST" ];then
     if [ -z "$ENNAME" ];then
 	ENNAME=eth0
     fi
-    localip=`ip addr show $ENNAME|grep "inet.*brd.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
+    localip=`ip addr show $ENNAME|grep "inet.*scope.*global.*$ENNAME"|awk '{print $2}'|awk -F/ '{print $1}'`
     zks=""
     i=1
     for server in `echo $ZKLIST|sed 's/\,/ /g'`;do
@@ -19,8 +19,14 @@ if [ ! -z "$ZKLIST" ];then
 	fi
 	let i=$i+1
     done
+    mkdir -p /data/zookeeper/{snapshot,zklog}
     echo "$ZKID" > /data/zookeeper/snapshot/myid && \
     sed -i 's/--serverlist--/'$zks'/g' $ZOOCFGDIR/zoo.cfg
+elif [ "$CONFIG_SERVER" ];then
+    export DM_READ_URI=`curl $CONFIG_SERVER/config/$(hostname)/zookeeper/filelist.json`
+    /data/run/DM_DOCKER_URI.py
+elif [ ! -z  "$DM_READ_URI" ];then
+    /data/run/DM_DOCKER_URI.py
 else
     sed -i 's/--serverlist--//g' $ZOOCFGDIR/zoo.cfg
 fi
