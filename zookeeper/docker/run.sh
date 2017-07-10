d702afe9f0b181613790a21cc87717620e39c961
ss75710541 <75710541@qq.com>
2017-07-10 11:06:33 +0800

    update zookeeper config

--- zookeeper/docker/run.sh
+++ zookeeper/docker/run.sh
@@ -1,10 +1,15 @@
-service=dataman-zookeeper
-
-                    docker run -d \
-                            -e ZOO_LOG_DIR=/data/logs/ \
-                            -e ZOO_LOG4J_PROP=INFO,CONSOLE \
-                            -e JAVA_OPTS="-Xmx512M -Xms256M"  \
-                            --name $service --net host --restart always \
-                            centos7/zookeeper-3.4.6
-                            #centos7/zookeeper-3.5.1
+docker rm -f dataman-zookeeper && \
+service=dataman-zookeeper && \
+MASTER_LIST=10.3.10.33 && \
+ENNAME=eth0 && \
+ZKLIST="$(echo $MASTER_LIST | sed "s/,/ /g" | sed "s/ /:2888:3888,/g"):2888:3888" && \
+docker run -it \
+       -e ZKLIST="$ZKLIST" \
+       -e ENNAME="$ENNAME" \
+       --name $service --net host --restart always \
+       --net host \
+       library/centos7-zookeeper-3.4.6
 #--entrypoint=bash \
+       #-e ZOO_LOG_DIR=/data/logs/ \
+       #-e ZOO_LOG4J_PROP=INFO,CONSOLE \
+       #-e JAVA_OPTS="-Xmx512M -Xms256M"  \
